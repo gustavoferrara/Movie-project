e661f81c12730d6fff4b14370a07c79efe61fc96
Gustavo Ferrara <guga.nicastro@gmail.com>
2022-05-17 13:09:54 -0300

    Updated navbar and global styles

--- src/helpers/getFeaturedTitles.ts
+++ src/helpers/getFeaturedTitles.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prefer-const */
 const getFeaturedTitles = (
   titlesRequested: number,
   minNumber: number,
@@ -28,61 +27,6 @@ const getFeaturedTitles = (
   }
 
   return titles;
-
-  // const checkMoviesAreEqual = (newMovie: number) => {
-  //   for (let otherMovie of movies) {
-  //     if (newMovie === otherMovie) {
-  //       newMovie = Math.floor(
-  //         Math.random() * (maxNumber - minNumber + 1) + minNumber,
-  //       );
-  //     } else {
-  //       return true;
-  //     }
-  //   }
-  // };
-
-  // if (secondMovie === firstMovie)
-  //   while (secondMovie === firstMovie) reassignMovie(secondMovie);
-
-  // if (thirdMovie === firstMovie || thirdMovie === secondMovie)
-  //   while (thirdMovie === firstMovie || thirdMovie === secondMovie)
-  //     reassignMovie(thirdMovie);
-
-  // // prettier-ignore
-  // if (fourthMovie === firstMovie || fourthMovie === secondMovie || fourthMovie === thirdMovie)
-  //   // prettier-ignore
-  //   while (fourthMovie === firstMovie || fourthMovie === secondMovie || fourthMovie === thirdMovie)
-  //     reassignMovie(fourthMovie);
-
-  // return [firstMovie, secondMovie, thirdMovie, fourthMovie];
 };
 
 export default getFeaturedTitles;
-
-// const getFeaturedMovies = (iterations: number, minNumber: number, maxNumber: number) => {
-//   const firstMovie = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
-//   let secondMovie = Math.floor(Math.random() * (19 + 1));
-//   let thirdMovie = Math.floor(Math.random() * (19 + 1));
-//   let fourthMovie = Math.floor(Math.random() * (19 + 1));
-
-//   const reassignMovie = (movieIndex: number) => {
-//     movieIndex = Math.floor(Math.random() * (19 + 1));
-//   };
-
-//   if (secondMovie === firstMovie)
-//     while (secondMovie === firstMovie) reassignMovie(secondMovie);
-
-//   if (thirdMovie === firstMovie || thirdMovie === secondMovie)
-//     while (thirdMovie === firstMovie || thirdMovie === secondMovie)
-//       reassignMovie(thirdMovie);
-
-//   // prettier-ignore
-//   if (fourthMovie === firstMovie || fourthMovie === secondMovie || fourthMovie === thirdMovie)
-//     // prettier-ignore
-//     while (fourthMovie === firstMovie || fourthMovie === secondMovie || fourthMovie === thirdMovie)
-//       reassignMovie(fourthMovie);
-
-//   return [firstMovie, secondMovie, thirdMovie, fourthMovie];
-// };
-
-// export default getFeaturedMovies;
