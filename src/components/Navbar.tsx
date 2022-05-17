299f55cf5a27125fd4c77077fe1d9fa120e8f143
Gustavo Ferrara <guga.nicastro@gmail.com>
2022-05-16 23:33:17 -0300

    Added navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,28 @@
+import Image from 'next/image';
 import Link from 'next/link';
+
+import styles from '@/styles/Navbar.module.scss';
 
 const Navbar = () => {
   return (
-    <nav className='navbar'>
-      <h1 className='navbar_title'>Blog website</h1>
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Image
+          src='/movielibrary.svg'
+          alt='Movie Library logo'
+          layout='fill'
+          objectFit='contain'
+        />
+      </div>
+      <div className={styles.mobilemenu}>
+        <Image
+          src='/mobilemenu.svg'
+          alt='Mobile menu'
+          layout='fill'
+          objectFit='contain'
+        />
+      </div>
+      {/* {isMobile ? <LinksDesktop /> : <HamburgerMenu />} */}
     </nav>
   );
 };
