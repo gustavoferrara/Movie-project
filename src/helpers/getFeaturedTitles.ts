9229d77b1f2e50cd3e0cd1bb546801bf043f6b29
Gustavo Ferrara <guga.nicastro@gmail.com>
2022-05-22 06:08:40 -0300

    Cleaned up getFeaturedTitles helper

--- src/helpers/getFeaturedTitles.ts
+++ src/helpers/getFeaturedTitles.ts
@@ -11,19 +11,15 @@ const getFeaturedTitles = (
     );
 
     if (titles.length !== 0) {
-      if (titles.some(otherTitle => newTitle === otherTitle)) {
-        while (titles.some(otherTitle => newTitle === otherTitle)) {
+      if (titles.some(existingTitle => newTitle === existingTitle)) {
+        while (titles.some(existingTitle => newTitle === existingTitle)) {
           newTitle = Math.floor(
             Math.random() * (maxNumber - minNumber + 1) + minNumber,
           );
         }
         titles.push(newTitle);
-      } else {
-        titles.push(newTitle);
-      }
-    } else {
-      titles.push(newTitle);
-    }
+      } else titles.push(newTitle);
+    } else titles.push(newTitle);
   }
 
   return titles;
